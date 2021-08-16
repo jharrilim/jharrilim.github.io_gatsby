af688cd11efb7e802931c1714e81a2cd6a230d7c
Joseph Harrison-Lim <Josephharrisonlim@gmail.com>
2021-08-15 23:29:50 -0400

    Gatsby fix for canvas

--- gatsby-node.js
+++ gatsby-node.js
@@ -41,3 +41,24 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
     });
   });
 };
+
+exports.onCreateWebpackConfig = ({
+  stage,
+  rules,
+  loaders,
+  plugins,
+  actions,
+}) => {
+  if (stage === "build-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /canvas/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    })
+  }
+};
