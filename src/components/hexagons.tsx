ff88323fe5ee7d9ae51103c49926b56eb4f0c178
Joe <Josephharrisonlim@gmail.com>
2021-08-16 08:53:36 -0400

    hide stroke

--- src/components/hexagons.tsx
+++ src/components/hexagons.tsx
@@ -40,8 +40,8 @@ const Hexagon: FC<Omit<RegularPolygonConfig, 'sides'>> = ({
         <animated.RegularPolygon
           sides={6}
           radius={20}
-          stroke="black"
-          strokeWidth={2}
+          {/*stroke="black"
+          strokeWidth={2}*/}
           onMouseEnter={() => setHover(true)}
           onMouseLeave={() => setHover(false)}
           onTouchStart={() => {
